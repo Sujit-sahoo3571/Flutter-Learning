718a8fe35fa5d0d880980e44c9add41072729540
chinku-3571 <chinku3571@gmail.com>
2022-01-19 08:51:10 +0530

    flutter hello world !

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Welcome Flutter",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Hello World"),
+          ),
+          body: Center(
+            child: Text(
+              "Hello World, Welcome To Flutter ",
+              style: TextStyle(fontSize: 35.0),
+            ),
+          )),
+    );
+  }
+}
